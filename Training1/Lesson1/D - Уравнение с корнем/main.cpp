82bd28ea8626b85dfbd3f0e9eff64d601f7fa2ec
Sergey Zabolotny <cepxuozab@gmail.com>
2023-03-27 09:32:34 +0300

    rename README

--- Training1/Lesson1/D - Уравнение с корнем/main.cpp
+++ Training1/Lesson1/D - Уравнение с корнем/main.cpp
@@ -3,27 +3,31 @@
 
 using namespace std;
 
-string solve(int a, int b, int c) {
-     if (c < 0) {
-       return "NO SOLUTION\n";    
-   } else if (a == 0) {
-       if (b == c * c) {
-           return "MANY SOLUTIONS\n";
-       } else {
-           return "NO SOLUTION\n"; 
-       }
-   } else {
-       if (!((c * c - b) % a)) {
-            return to_string((c * c - b) / a);    
-       } else {
-           return "NO SOLUTION\n";
-       }
-   }
-
+auto solve(int a, int b, int c) -> string
+{
+    if (c < 0) {
+        return "NO SOLUTION\n";
+    }
+    if (a == 0) {
+        if (b == c * c) {
+            return "MANY SOLUTIONS\n";
+        } else {
+            return "NO SOLUTION\n";
+        }
+    } else {
+        if (!((c * c - b) % a)) {
+            return to_string((c * c - b) / a);
+        } else {
+            return "NO SOLUTION\n";
+        }
+    }
 }
 
-int main() {  
-    int a, b, c;
+auto main() -> int
+{
+    int a;
+    int b;
+    int c;
     cin >> a >> b >> c;
     cout << solve(a, b, c);
     return 0;
---
+++ Training1/Lesson1/README.md
@@ -0,0 +1,3 @@
+[Contest 1]
+
+[Contest 1]: <https://contest.yandex.ru/contest/27393/problems/>
