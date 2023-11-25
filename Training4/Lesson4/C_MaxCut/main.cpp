6fa459c979e69d22a74523981fce822096940efc
cepxuozab <cepxuozab@gmail.com>
2023-11-25 10:23:09 +0300

    finish lesson4

--- Training4/Lesson4/C_MaxCut/main.cpp
+++ Training4/Lesson4/C_MaxCut/main.cpp
@@ -1,102 +1,47 @@
-#include <iostream>
-#include <optional>
-#include <queue>
+#include <cmath>
+#include <cstdio>
 #include <vector>
 
-using Graph = std::vector<std::vector<int>>;
-using Opt = std::optional<int>;
-bool bfs(Graph const& graph, std::vector<Opt> const& pair_u, std::vector<Opt> const& pair_v,
-    std::vector<int>& dist)
+int main()
 {
-    int const INF = 1'000'000'007;
-    std::queue<int> queue;
-    int const size = graph.size();
-    for (int u = 0; u < size; ++u) {
-        if (!pair_u[u].has_value()) {
-            dist[u] = 0;
-            queue.push(u);
-        } else {
-            dist[u] = INF;
+    int numberOfVertices;
+    scanf("%d", &numberOfVertices);
+    std::vector<std::vector<int>> adjacencyMatrix(numberOfVertices);
+    for (int i = 0; i < numberOfVertices; ++i) {
+        adjacencyMatrix[i].resize(numberOfVertices);
+        for (int j = 0; j < numberOfVertices; ++j) {
+            scanf("%d", &adjacencyMatrix[i][j]);
         }
     }
-    dist[size] = INF;
-    while (!queue.empty()) {
-        auto u = queue.front();
-        queue.pop();
-        if (dist[u] < dist[size]) {
-            for (int v = 0; v < size; ++v) {
-                if (graph[u][v] != 0 && dist[pair_v[v].value()] == INF) {
-                    dist[pair_v[v].value()] = dist[u] + 1;
-                    queue.push(pair_v[v].value());
+    int maxValue = (int)pow(2, numberOfVertices);
+    int maxPermutation = 0;
+    int maxLength = 0;
+    auto doCut = [&]() {
+        for (int i = 1; i < maxValue - 1; i++) {
+            int currentLength = 0;
+            int mask = 0xFFFFFFFF >> (32 - numberOfVertices);
+            if (maxPermutation != (i ^ mask)) {
+                for (int j = 0; j < numberOfVertices; j++) {
+                    int maskBit = 1 << j;
+                    if ((i & maskBit) == 0) {
+                        for (int k = 0; k < numberOfVertices; k++) {
+                            if (((i >> k) & 1) == 1) {
+                                currentLength += adjacencyMatrix[j][k];
+                            }
+                        }
+                    }
                 }
             }
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+                maxPermutation = i;
+            }
         }
+    };
+    doCut();
+    printf("%d\n", maxLength);
+    for (int i = 0; i < numberOfVertices; i++) {
+        int ans = ((maxPermutation >> i) & 1) == 1 ? 2 : 1;
+        printf("%d ", ans);
     }
-    return dist[size] != INF;
 }
-
-/*
-from queue import Queue
-
-def bfs(graph, pair_u, pair_v, dist):
-    q = Queue()
-    for u in range(len(graph)):
-        if pair_u[u] is None:
-            dist[u] = 0
-            q.put(u)
-        else:
-            dist[u] = float('inf')
-    dist[len(graph)] = float('inf')
-    while not q.empty():
-        u = q.get()
-        if dist[u] < dist[len(graph)]:
-            for v in range(len(graph)):
-                if graph[u][v] and dist[pair_v[v]] == float('inf'):
-                    dist[pair_v[v]] = dist[u] + 1
-                    q.put(pair_v[v])
-    return dist[len(graph)] != float('inf')
-
-def dfs(graph, pair_u, pair_v, dist, u):
-    if u is not None:
-        for v in range(len(graph)):
-            if graph[u][v] and dist[pair_v[v]] == dist[u] + 1 and dfs(graph, pair_u, pair_v, dist, pair_v[v]):
-                pair_v[v] = u
-                pair_u[u] = v
-                return True
-        dist[u] = float('inf')
-        return False
-    return True
-
-def hopcroft_karp(graph):
-    pair_u = [None] * len(graph)
-    pair_v = [None] * len(graph[0])
-    dist = [float('inf')] * (len(graph) + 1)
-    matching = 0
-    while bfs(graph, pair_u, pair_v, dist):
-        for u in range(len(graph)):
-            if pair_u[u] is None and dfs(graph, pair_u, pair_v, dist, u):
-                matching += 1
-    return pair_u, matching
-
-def divide_vertices(n, graph):
-    # Находим максимальное паросочетание в двудольном графе
-    pair_u, matching = hopcroft_karp(graph)
-    # Разделяем вершины на две доли
-    part = [0] * n
-    for u in range(n):
-        if pair_u[u] is not None:
-            part[u] = 1
-    # Считаем сумму весов ребер, соединяющих вершины из разных долей
-    weight = 0
-    for u in range(n):
-        for v in range(n):
-            if graph[u][v] and part[u] != part[v]:
-                weight += graph[u][v]
-    return weight, part
-
-n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-weight, part = divide_vertices(n, graph)
-print(weight)
-print(' '.join(str(p + 1) for p in part))
-*/
