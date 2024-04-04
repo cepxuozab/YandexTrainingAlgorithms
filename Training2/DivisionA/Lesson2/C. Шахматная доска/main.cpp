901d552ee935045eafdc61c8c94d60b6e7ce7d53
cepxuozab <cepxuozab@gmail.com>
2024-04-04 08:18:00 +0300

    modify some solutions

--- Training2/DivisionA/Lesson2/C. Шахматная доска/main.cpp
+++ Training2/DivisionA/Lesson2/C. Шахматная доска/main.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <string>
 
-int main()
-{
-    int n, k[64][3];
-    cin >> n;
-    for (int z = 0; z < n; z++) {
-        cin >> k[z][1] >> k[z][2];
-        k[z][0] = 4;
+constexpr int SIZE = 10;
+
+auto ReadBoards(int n) -> std::vector<std::string> {
+    std::vector<std::string> chess_board(SIZE, std::string(SIZE, ' '));
+    for (int i = 0; i < n; ++i) {
+        int x, y;
+        std::cin >> x >> y;
+        chess_board[x][y] = '1';
     }
-    if (n == 1) {
-        cout << 4;
-        return 0;
+    return chess_board;
+}
+
+auto NeighbourCut(std::vector<std::string> const &chess_board, int x, int y) -> int {
+    int count = 0;
+    std::vector<std::pair<int, int>> diff{{-1, 0},
+                                          {0,  -1},
+                                          {0,  1},
+                                          {1,  0}};
+    for (auto &[dx, dy]: diff) {
+        count += (chess_board[x + dx][y + dy] == ' ');
     }
-    for (int z = 0; z < n - 1; z++)
-        for (int x = z + 1; x < n; x++) {
-            if ((abs(k[z][1] - k[x][1]) == 1
-                    && k[z][2] - k[x][2] == 0)
-                || ((abs(k[z][2] - k[x][2]) == 1
-                    && k[z][1] - k[x][1] == 0))) {
-                k[z][0]--;
-                k[x][0]--;
+    return count;
+}
+
+auto PerimeterCut(std::vector<std::string> const &chess_board) -> int {
+    int perimeter = 0;
+    for (int x = 0; x < SIZE; ++x) {
+        auto const &line = chess_board[x];
+        for (int y = 0; y < SIZE; ++y) {
+            if (line[y] == '1') {
+                perimeter += NeighbourCut(chess_board, x, y);
             }
         }
-    int s = 0;
-    for (int z = 0; z < n; z++)
-        s += k[z][0];
-    cout << s;
+    }
+    return perimeter;
+}
 
-    return 0;
+auto main() -> int {
+    int n;
+    std::cin >> n;
+    std::cout << PerimeterCut(ReadBoards(n));
 }
