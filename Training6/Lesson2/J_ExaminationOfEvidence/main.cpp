7f25d464df2599d4cf5a075ae74def3b9dab382d
cepxuozab <cepxuozab@gmail.com>
2024-11-04 10:34:23 +0300

    add taskJ lesson2

--- Training6/Lesson2/J_ExaminationOfEvidence/main.cpp
+++ Training6/Lesson2/J_ExaminationOfEvidence/main.cpp
@@ -1,81 +1,43 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+
+template <typename T>
+auto operator>>(std::istream& in, std::vector<T>& v) -> std::istream& {
+    for (auto& item : v) in >> item;
+    return in;
+}
 
 auto main() -> int {  // NOLINT
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int n;
-    cin >> n;
-    vector<int> a(n);
-    for (auto& x : a) cin >> x;
+    std::cin >> n;
 
+    std::vector<int> a(n);
+    std::cin >> a;
     int m, k;
-    cin >> m >> k;
-    vector<int> queries(m);
-    for (auto& x : queries) cin >> x;
+    std::cin >> m >> k;
+    std::vector<int> x(m);
+    std::cin >> x;
 
-    const int LOG = 20;
-    vector<vector<int>> jump(LOG, vector<int>(n, 0));
-    vector<vector<int>> count(LOG, vector<int>(n, 0));
-
-    for (int x = 0; x < n; ++x) {
-        if (x == 0) {
-            jump[0][x] = x;
-            count[0][x] = 0;
+    std::vector<int> left(n, 0);
+    int cnt = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i - 1] < a[i])
+            left[i] = left[i - 1];
+        else if (a[i - 1] > a[i]) {
+            left[i] = i;
+            cnt = 0;
         } else {
-            if (a[x - 1] <= a[x]) {
-                jump[0][x] = x - 1;
-                if (a[x - 1] == a[x]) {
-                    count[0][x] = 1;
-                } else {
-                    count[0][x] = 0;
-                }
-            } else {
-                jump[0][x] = x;
-                count[0][x] = 0;
+            cnt++;
+            left[i] = left[i - 1];
+            while (cnt > k) {
+                cnt -= a[left[i]] == a[left[i] + 1];
+                left[i]++;
             }
         }
     }
 
-    for (int j = 1; j < LOG; ++j) {
-        for (int x = 0; x < n; ++x) {
-            int prev = jump[j - 1][x];
-            jump[j][x] = jump[j - 1][prev];
-            if (count[j - 1][x] >= (k + 1) || count[j - 1][prev] >= (k + 1)) {
-                count[j][x] = k + 1;
-            } else {
-                count[j][x] = count[j - 1][x] + count[j - 1][prev];
-                if (count[j][x] > k) {
-                    count[j][x] = k + 1;
-                }
-            }
-        }
+    for (int xi : x) {
+        std::cout << left[xi - 1] + 1 << ' ';
     }
-
-    vector<int> res;
-    res.reserve(m);
-
-    for (int i = 0; i < m; ++i) {
-        int q = queries[i];
-        int x = q - 1;
-        int remaining_k = k;
-        for (int j = LOG - 1; j >= 0; --j) {
-            if (jump[j][x] != x && count[j][x] <= remaining_k) {
-                remaining_k -= count[j][x];
-                x = jump[j][x];
-                if (remaining_k < 0) break;
-            }
-        }
-        res.push_back(x + 1);
-    }
-
-    for (int i = 0; i < m; ++i) {
-        if (i > 0) cout << ' ';
-        cout << res[i];
-    }
-    cout << '\n';
-
-    return 0;
+    std::cout << '\n';
 }
