145f699f56bcc48d3f6883e21acc4c7b9622f4d8
cepxuozab <cepxuozab@gmail.com>
2025-06-23 06:55:52 +0300

    add task4

--- Training7/D_Output_the_maximum_cost_route/main.cpp
+++ Training7/D_Output_the_maximum_cost_route/main.cpp
@@ -1,25 +1,91 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-int main()
+using Grid = std::vector<std::vector<int>>;
+
+auto ReadGrid() -> Grid;
+
+auto Preprocessind(Grid const& grid) -> Grid;
+
+auto PathRecovery(Grid const& grid, Grid const& dp, std::vector<char>& answer) -> int64_t;
+
+auto main() -> int
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    auto const grid = ReadGrid();
+    auto const dp = Preprocessind(grid);
+    std::vector<char> answer;
+    auto sum = PathRecovery(grid, dp, answer);
+    std::cout << sum << std::endl;
+    for (char ch : answer) {
+        std::cout << ch << ' ';
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
+auto ReadGrid() -> Grid
 {
     int n;
-    std::cin >> n;
-    std::vector<int> a(n + 1), dp(n + 1, 0);
-    for (int i = 1; i <= n; ++i)
-    {
-        std::cin >> a[i];
+    int m;
+    std::cin >> n >> m;
+    Grid grid(n, std::vector<int>(m, 0));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            std::cin >> grid[i][j];
+        }
     }
-    std::ranges::sort(a);
-    dp[2] = a[2] - a[1];
-    dp[3] = a[3] - a[1];
+    return grid;
+}
 
-    for (int i = 4; i <= n; i++)
-    {
-        dp[i] = std::min(dp[i - 1], dp[i - 2]) + a[i] - a[i - 1];
+auto Preprocessind(Grid const& grid) -> Grid
+{
+    size_t n = grid.size();
+    size_t m = grid.front().size();
+    Grid dp(n, std::vector<int>(m, 0));
+    dp[0][0] = grid[0][0];
+    for (int i = 1; i < n; ++i) {
+        dp[i][0] = dp[i - 1][0] + grid[i][0];
     }
-    std::cout << dp.back();
+    for (int j = 1; j < m; ++j) {
+        dp[0][j] = dp[0][j - 1] + grid[0][j];
+    }
+    for (int i = 1; i < n; ++i) {
+        for (int j = 1; j < m; ++j) {
+            dp[i][j] = grid[i][j] + std::max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp;
+}
 
-    return 0;
+auto PathRecovery(Grid const& grid, Grid const& dp, std::vector<char>& answer) -> int64_t
+{
+    int n = grid.size() - 1;
+    int m = grid.front().size() - 1;
+    int64_t sum = grid[0][0];
+
+    while (n > 0 && m > 0) {
+        sum += grid[n][m];
+        if (dp[n][m] - grid[n][m] == dp[n - 1][m]) {
+            answer.push_back('D');
+            n--;
+        } else {
+            answer.push_back('R');
+            m--;
+        }
+    }
+    while (n > 0) {
+        sum += grid[n][0];
+        answer.push_back('D');
+        n--;
+    }
+    while (m > 0) {
+        sum += grid[0][m];
+        answer.push_back('R');
+        m--;
+    }
+    std::reverse(answer.begin(), answer.end());
+    return sum;
 }
