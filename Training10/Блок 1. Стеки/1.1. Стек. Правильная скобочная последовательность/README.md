249dc1b2f56a730f8b50e605cd3743c4dd7403bd
cepxuozab <cepxuozab@gmail.com>
2026-09-08 18:27:32 +0300

    add readme training3 divB

--- Training10/Блок 1. Стеки/1.1. Стек. Правильная скобочная последовательность/README.md
+++ Training10/Блок 1. Стеки/1.1. Стек. Правильная скобочная последовательность/README.md
@@ -2,6 +2,6 @@
 
 [A. Правильная скобочная последовательность.](https://contest.yandex.ru/contest/45468/problems/12/)  
 
-[B. Минимальная ПСП](https://new.contest.yandex.ru/contests/89529/problems?id=30404%2F2024_11_02%2FmyVIGk5VNm)  
+[B. Минимальная ПСП](https://contest.yandex.ru/contest/66794/problems/F/)  
 
 [C. Поврежденный XML](https://contest.yandex.ru/contest/45469/problems/15/)
--- Training3/DivisionB/Task15.cpp
+++ Training3/DivisionB/Task15.cpp
@@ -1,39 +1,35 @@
-#include <fstream>
 #include <iostream>
 #include <stack>
-#include <utility>
 #include <vector>
 
-int main()
-{
-    std::vector<std::pair<int, int>> cities;
 
-    std::fstream file;
-    file.open("input.txt");
+struct City {
+    int accommodation_price{};
+    int id{};
+};
 
-    int citiesCount;
-    file >> citiesCount;
-
-    for (int i = 0; i != citiesCount; i++) {
-        int input;
-        file >> input;
-        cities.push_back({ input, i });
+auto main() -> int {
+    std::vector<City> cities;
+    int city_count{};
+    std::cin >> city_count;
+    for (int i = 0; i < city_count; ++i) {
+        City city;
+        std::cin >> city.accommodation_price;
+        city.id = i;
+        cities.push_back(city);
     }
-
-    std::vector<int> moving;
-    std::vector<int> result(citiesCount, -1);
-
-    for (const auto& pair : cities) {
-        while (!moving.empty() && (cities[moving.back()].first > pair.first)) {
-            result[moving.back()] = pair.second;
-            moving.pop_back();
+    std::stack<int> moving;
+    std::vector<int> result(cities.size(), -1);
+    for (auto const &[price, id] : cities) {
+        while (!moving.empty() &&
+               cities[moving.top()].accommodation_price > price) {
+            result[moving.top()] = id;
+            moving.pop();
         }
-        moving.push_back(pair.second);
+        moving.push(id);
     }
-
-    for (auto elem : result) {
-        std::cout << elem << " ";
+    for (int i = 0; i < result.size(); ++i) {
+        std::cout << result[i] << " ";
     }
-
     return 0;
 }
